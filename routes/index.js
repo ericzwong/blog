59f27e8a877b6ef398def716f61870aa91926f1d
wangzhe <wangzhegoo@gmail.com>
2015-05-16 13:04:43 +0800

    添加了翻页功能

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,27 @@ module.exports = function(app){
             res.render('index');
         });
 
-        //获取所有的Article
-        app.get('/ajax/article/all',function(req,res){
+        app.get('/page/:page',function(req,res){
+            //res.writeHead(200,{"Content-Type":"text/html"});
+            //res.end(fs.readFileSync(path.join(__dirname,'../views/index.html'),'utf8'));
+            res.render('index');
+        });
 
-            mysqlConnection.query('SELECT article_id,container,modify_time,title FROM articles',function(err,result){
+        //获取所有的Article
+        app.get('/ajax/article/all/page/:page',function(req,res){
+
+            var paramsPage = req.params['page'];
+            var page = 0;
+            //设定每页显示五条
+            var seek = 5;
+
+            if((!isNaN(parseInt(paramsPage))) && parseInt(paramsPage) > 0){
+                page = parseInt(paramsPage) - 1;
+            }
+
+            var startSeek = page * seek;
+
+            mysqlConnection.query('SELECT article_id,container,modify_time,title FROM articles LIMIT ?,?',[startSeek,seek],function(err,result){
                 if(err){
                     return res.end('{"msg":"从数据库当中获取失败"}');
                 }
