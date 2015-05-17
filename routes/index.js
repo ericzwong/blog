91004bce3d345f9174bda8685824ae83db0b41fc
wangzhe <wangzhegoo@gmail.com>
2015-05-17 11:32:08 +0800

    让搜索引擎可以抓取内容了

--- routes/index.js
+++ routes/index.js
@@ -10,35 +10,75 @@ var mysqlConnection = mysql.createConnection({
 
 mysqlConnection.connect();
 
+//从数据库当中获取文章
+function getArticle(paramsPage,callback){
+    var page = 0;
+    //设定每页显示五条
+    var seek = 5;
+
+    if((!isNaN(parseInt(paramsPage))) && parseInt(paramsPage) > 0){
+        page = parseInt(paramsPage) - 1;
+    }
+
+    var startSeek = page * seek;
+
+    mysqlConnection.query('SELECT article_id,container,modify_time,title FROM articles LIMIT ?,?',[startSeek,seek],function(err,result){
+        if(err){
+            return callback(new Error("从数据中获取失败"),null);
+        }
+
+        return callback(null,result);
+    });
+}
+
 module.exports = function(app){
 
         app.get('/',function(req,res){
             //res.writeHead(200,{"Content-Type":"text/html"});
             //res.end(fs.readFileSync(path.join(__dirname,'../views/index.html'),'utf8'));
-            res.render('index');
+
+            getArticle(1,function(err,result){
+
+                var content = "";
+
+                if(err){
+                    content = "<p>从数据库当中获取失败</p>";
+                }else{
+                    content = result;
+                }
+
+                res.render('index',{"content":content,"nextPage": 3,"prevPage":1});
+
+            });
+
+
         });
 
         app.get('/page/:page',function(req,res){
-            //res.writeHead(200,{"Content-Type":"text/html"});
-            //res.end(fs.readFileSync(path.join(__dirname,'../views/index.html'),'utf8'));
-            res.render('index');
+
+            var paramsPage = req.params['page'];
+
+            getArticle(paramsPage,function(err,result){
+
+                var content = "";
+
+                if(err){
+                    content = "<p>从数据库当中获取失败</p>";
+                }else{
+                    content = result;
+                }
+
+                res.render('index',{"content":content,"nextPage": parseInt(paramsPage) + 1,"prevPage":paramsPage - 1});
+
+            });
         });
 
         //获取所有的Article
         app.get('/ajax/article/all/page/:page',function(req,res){
 
             var paramsPage = req.params['page'];
-            var page = 0;
-            //设定每页显示五条
-            var seek = 5;
 
-            if((!isNaN(parseInt(paramsPage))) && parseInt(paramsPage) > 0){
-                page = parseInt(paramsPage) - 1;
-            }
-
-            var startSeek = page * seek;
-
-            mysqlConnection.query('SELECT article_id,container,modify_time,title FROM articles LIMIT ?,?',[startSeek,seek],function(err,result){
+            getArticle(paramsPage,function(err,result){
                 if(err){
                     return res.end('{"msg":"从数据库当中获取失败"}');
                 }
